691cfa087f77b67a53be6107154a21be281e8898
unknown <prathmeshd2004@gmail.com>
2026-05-05 23:11:53 +0530

    Run migrations on WSGI startup

--- smartallot/wsgi.py
+++ smartallot/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartallot.settings')
+
+# Ensure the production database schema is initialized before the app starts serving.
+django.setup()
+call_command('migrate', interactive=False, run_syncdb=True, verbosity=0)
 
 application = get_wsgi_application()
